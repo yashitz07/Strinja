a7ca7b1207383efccb5fea41f32220d3bae01a4f
YashPatel <yashitz07@gmail.com>
2024-01-06 10:34:35 +0530

    Add .gitignore file and .env file

--- mern-server/index.js
+++ mern-server/index.js
@@ -1,7 +1,7 @@
 
 const express = require('express')
 const app = express()
-const port = process.env.port || 5000
+const port = process.env.PORT || 5000
 const cors = require('cors')
 require('dotenv').config()
 // middleware
